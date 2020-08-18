9ca7cbb2aef66d9340af313dcc6a991074bdcc84
grandmasteriVan <grandmaster.ivan@gmail.com>
2020-08-18 14:42:46 +0300

    Update promXMLmod.php

--- promXMLmod.php
+++ promXMLmod.php
@@ -93,6 +93,16 @@ class testXML
         return $paramVal;
     }
 
+    private function getFirstParamVal($param)
+    {
+        if (preg_match("#>(.+?)<#",$param,$matches))
+        {
+            $paramVal=$matches[1];
+        }
+        $firstParamVal=explode(";",$paramVal);
+        return $firstParamVal[0];
+    }
+
     private function getParams($item)
     {
         //var_dump ($item);
@@ -289,27 +299,336 @@ class testXML
                         $param_new=str_ireplace("Для мужчин","Мужской",$param_new);
                         $param_new=str_ireplace("Женский;Мужской","Унисекс",$param_new);
                         $param_new=str_ireplace("Мужской;Женский","Унисекс",$param_new);
+                        $param_new=str_ireplace("Унисекс;Для пары","Унисекс",$param_new);
                     }
-                    //вот тут надо из названия позиции вытащить тематику
-                    $itemName=strtolower($itemName);
+
                     if (strcmp($paramName,"Цвет")==0)
                     {
+                        //echo "вошли в цвет<br>$paramVal<br>";
+                        $firstParamVal=$this->getFirstParamVal($param);
+                        $param="<param name=\"Цвет\">".$firstParamVal."</param>";
                         $param_new=str_ireplace("Золотой","Золотистый",$param);
                         $param_new=str_ireplace("Салатовый","Зеленый",$param_new);
                     }
                     if (strcmp($paramName,"Материал")==0)
                     {
+                        
                         $param_new=str_ireplace("Материал","Тип ткани",$param);
+                        $firstParamVal=$this->getFirstParamVal($param);
+                        $param_new="<param name=\"Тип ткани\">".$firstParamVal."</param>";
                         $param_new=str_ireplace("Кожзаменитель","Искусственная кожа",$param_new);
                     }
                     if (strcmp($paramName,"Размер")==0)
                     {
+                        
                         $param_new=str_ireplace("Размер","Международный размер",$param);
-                        $param_new=str_ireplace("One size","S\/M\/L",$param_new);
+                        $param_new=str_ireplace("One size","S/M/L",$param_new);
+                        $param_new=str_ireplace(";","/",$param_new);
                     }
                     $params_new[]=$param_new;
                 }
                 //а тут мы будем прописывать захардкодженные параметры
+                 //вот тут надо из названия позиции вытащить тематику
+                 $itemName=" ".mb_strtolower($itemName);
+                 
+                 if (strripos($itemName,"ангел"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Ангелы, Демоны</param>";
+                 }
+                 if (strripos($itemName,"дьявол"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Ангелы, Демоны</param>";
+                 }
+                 if (strripos($itemName,"черт"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Ангелы, Демоны</param>";
+                 }
+                 //Гангстеры, Заключенные, Кабаре
+                 if (strripos($itemName,"заключ"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Гангстеры, Заключенные, Кабаре</param>";
+                 }
+                 if (strripos($itemName,"заключ"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Гангстеры, Заключенные, Кабаре</param>";
+                 }
+                 if (strripos($itemName,"мафи"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Гангстеры, Заключенные, Кабаре</param>";
+                 }
+                 if (strripos($itemName,"танцовщ"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Гангстеры, Заключенные, Кабаре</param>";
+                 }
+                 if (strripos($itemName,"боди из чёрно белых полосок"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Гангстеры, Заключенные, Кабаре</param>";
+                 }
+                 //Горничные
+                 if (strripos($itemName,"горничн"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Горничные</param>";
+                 }
+                 if (strripos($itemName,"гуверн"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Горничные</param>";
+                 }
+                 if (strripos($itemName,"чистоты"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Горничные</param>";
+                 }
+                 if (strripos($itemName,"дворецк"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Горничные</param>";
+                 }
+                 if (strripos($itemName,"домраб"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Горничные</param>";
+                 }
+                 if (strripos($itemName,"служан"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Горничные</param>";
+                 }
+                 if (strripos($itemName,"домохоз"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Горничные</param>";
+                 }
+                 //Дикий Запад
+                 if (strripos($itemName,"ковбой"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Дикий Запад</param>";
+                 }
+                 //Другие образы
+                 if (strripos($itemName,"маска"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Другие образы</param>";
+                 }
+                 if (strripos($itemName,"укротительница"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Другие образы</param>";
+                 }
+
+                 //Животные
+                 if (strripos($itemName,"кошк"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Животные</param>";
+                 }
+                 if (strripos($itemName,"кошеч"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Животные</param>";
+                 }
+                 if (strripos($itemName,"леопар"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Животные</param>";
+                 }
+                 if (strripos($itemName,"зайч"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Животные</param>";
+                 }
+                 if (strripos($itemName,"зайк"))
+                 {                 
+                     $params_new[]="<param name=\"Тематика костюма\">Животные</param>";
+                 }
+                 if (strripos($itemName,"плейбой"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Животные</param>";
+                 }
+                 if (strripos($itemName,"крол"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Животные</param>";
+                 }
+                 //Исторические
+                 if (strripos($itemName,"нила"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Животные</param>";
+                 }
+                 //Медперсонал
+                 if (strripos($itemName,"медсес"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Медперсонал</param>";
+                 }
+                 if (strripos($itemName,"доктор"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Медперсонал</param>";
+                 }
+                 //Монашки, Священники
+                 if (strripos($itemName,"монаш"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Монашки, Священники</param>";
+                 }
+                 //Морские образы
+                 if (strripos($itemName,"моря"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Морские образы</param>";
+                 }
+                 if (strripos($itemName,"морск"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Морские образы</param>";
+                 }
+                 //Национальные
+                 //Новый Год
+                 if (strripos($itemName,"новогод"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Новый Год</param>";
+                 }
+                 if (strripos($itemName,"снегур"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Новый Год</param>";
+                 }
+                 if (strripos($itemName,"сант"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Новый Год</param>";
+                 }
+                 //Пилоты, Стюардессы
+                 if (strripos($itemName,"стюард"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Пилоты, Стюардессы</param>";
+                 }
+                 if (strripos($itemName,"полет"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Пилоты, Стюардессы</param>";
+                 }
+                 if (strripos($itemName,"борт-пров"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Пилоты, Стюардессы</param>";
+                 }
+                 if (strripos($itemName,"пилот"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Пилоты, Стюардессы</param>";
+                 }
+                 //Пираты
+                 if (strripos($itemName,"пират"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Пираты</param>";
+                 }
+                 //Пожарные
+                 if (strripos($itemName,"пожар"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Пожарные</param>";
+                 }
+                 //Полиция, военные
+                 if (strripos($itemName,"полиц"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Полиция, военные</param>";
+                 }
+                 if (strripos($itemName,"policem"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Полиция, военные</param>";
+                 }
+                 if (strripos($itemName,"сержант"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Полиция, военные</param>";
+                 }
+                 if (strripos($itemName,"шериф"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Полиция, военные</param>";
+                 }
+                 if (strripos($itemName,"военн"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Полиция, военные</param>";
+                 }
+                 if (strripos($itemName,"камуфл"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Полиция, военные</param>";
+                 }
+                 if (strripos($itemName,"в бой"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Полиция, военные</param>";
+                 }
+                 if (strripos($itemName,"милитари"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Полиция, военные</param>";
+                 }
+                 if (strripos($itemName,"гусар"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Полиция, военные</param>";
+                 }
+                 //Рабочая униформа
+                 if (strripos($itemName,"официан"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Рабочая униформа</param>";
+                 }
+                 if (strripos($itemName,"повар"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Рабочая униформа</param>";
+                 }
+                 if (strripos($itemName,"механик"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Рабочая униформа</param>";
+                 }
+                 if (strripos($itemName,"секретар"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Рабочая униформа</param>";
+                 }
+                 //Свадьба
+                 if (strripos($itemName,"невест"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Свадьба</param>";
+                 }
+                 //Сказочные герои, Киногерои
+                 if (strripos($itemName,"белосн"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Сказочные герои, Киногерои</param>";
+                 }
+                 if (strripos($itemName,"восточная"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Сказочные герои, Киногерои</param>";
+                 }
+                 if (strripos($itemName,"королев"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Сказочные герои, Киногерои</param>";
+                 }
+                 //Спорт
+                 if (strripos($itemName,"гонщиц"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Спорт</param>";
+                 }
+                 if (strripos($itemName,"гоночн"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Спорт</param>";
+                 }
+                 //Супергерои
+                 if (strripos($itemName,"супер"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Супергерои</param>";
+                 }
+                 if (strripos($itemName,"super"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Супергерои</param>";
+                 }
+                 if (strripos($itemName,"бетг"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Супергерои</param>";
+                 }
+                 if (strripos($itemName,"amazon"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Супергерои</param>";
+                 }
+                 //Хеллоуин
+                 //Школа
+                 if (strripos($itemName,"школьн"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Школа</param>";
+                 }
+                 if (strripos($itemName,"студент"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Школа</param>";
+                 }
+                 if (strripos($itemName,"учитель"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Школа</param>";
+                 }
+                 if (strripos($itemName,"учениц"))
+                 {
+                     $params_new[]="<param name=\"Тематика костюма\">Школа</param>";
+                 }
+                 /*echo "<b>$itemName</b><br>";
+                 echo "<pre>";
+                 print_r($params_new);
+                 echo "</pre>";*/
+
+                 //на всякий случай удаляем возможные дубли
+                 $params_new=array_unique($params_new);
                 //а теперь собираем айтем (старую шапку+новые параметры)
                 //сначала склеиваем параметры
                 foreach ($params_new as $new_param)
@@ -336,8 +655,9 @@ class testXML
         //начинаем собирать финальную ХМЛку
         $XMLnew=$xmlHead.$items_new;
         $XMLnew=$this->delSpaces($XMLnew);
-        var_dump($XMLnew);
+        //var_dump($XMLnew);
         file_put_contents("new_test.xml",$XMLnew);
+        echo "<b>Done</b>";
 
     }
     
